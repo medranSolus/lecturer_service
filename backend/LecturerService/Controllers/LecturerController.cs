f2495d8a3caa98a022d8eabe4c50d41ab57b7389
Marek Machliński <53278476+medranSolus@users.noreply.github.com>
2021-05-06 17:52:37 +0200

    Added missing using in LecturerController.

--- backend/LecturerService/Controllers/LecturerController.cs
+++ backend/LecturerService/Controllers/LecturerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 namespace LecturerService.Controllers
 {
